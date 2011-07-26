7705da39e6b2626150676eb12c33f9393f1d2365
Jan Schlie <info@janschlie.de>
2011-07-26 22:17:17 +0200

    installed ruby on rails and added new rails project

---
+++ nerdism/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nerdism::Application.config.secret_token = '56fde07eea6bad259691c625867bb6e4e620106ade91c75e3969265149adff9100cde5986b668139113e3040df1c0a5f6c2704c48b3d12c1daeb681d419c07d8'
